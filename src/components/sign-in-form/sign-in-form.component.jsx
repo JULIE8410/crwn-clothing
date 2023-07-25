dd56f7a64db12d4686397fa134d85e93ff1620e8
Julie <aqua8410@gmail.com>
2023-07-25 10:27:59 +0900

    adding redirect for netlify

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,93 +1,97 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
-import { 
-    signInWithGooglePopup,
-    createUserDocumentFromAuth,
-    signInAuthUserWithEmailAndPassword
-} from "../../utils/firebase/firebase.utils";
-import './sign-in-form.styles.scss';
 
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+import "./sign-in-form.styles.scss";
 
 const defaultFormFields = {
-    email: '',
-    password: '',
+  email: "",
+  password: "",
 };
 
 const SignInForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const { email, password } = formFields;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
 
-    const signInWithGoogle = async () => {
-        await signInWithGooglePopup();
-        //setCurrentUser(user);
-        
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+    //setCurrentUser(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await signInAuthUserWithEmailAndPassword(email, password);
+      console.log(user);
+
+      // setCurrentUser(user);
+
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+      }
     }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
 
-        try {
-            const user = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(user);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-           // setCurrentUser(user);
-            
-            resetFormFields();
+    setFormFields({ ...formFields, [name]: value });
+  };
 
-        } catch (error) {
-            switch(error.code) {
-                case "auth/wrong-password" :
-                    alert('incorrect password for email');
-                break;
-                case "auth/user-not-found" :
-                    alert('no user associated with this email');
-                break;
-                default :
-                console.log(error);
-            }
-        }
-    }
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
-
-    const resetFormFields = () => {
-        setFormFields(defaultFormFields);
-    }
-
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-
-        setFormFields({...formFields, [name]: value});
-    };
-    
-    return (
-        <div className="sign-up-container">
-            <h2>Already have an account?</h2>
-            <span>Sign in with your email and password</span>
-            <form onSubmit={handleSubmit}>
-                
-                <FormInput 
-                    label="Email" 
-                    type="email" 
-                    required 
-                    onChange={handleChange} 
-                    name="email"
-                    value={email}/>
-
-                <FormInput 
-                    label="Password"
-                    type="password" 
-                    required 
-                    onChange={handleChange} 
-                    name="password"
-                    value={password}/>
-                <div className="buttons-container">
-                  <Button type="submit">Sign In</Button>
-                  <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
-                </div>
-            </form>
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button
+            type="button"
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={signInWithGoogle}
+          >
+            Sign In With oogle
+          </Button>
         </div>
-    );
-}
+      </form>
+    </div>
+  );
+};
 
 export default SignInForm;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,35 +1,6 @@
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
-
-const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-];
-
   return (
     <div>
      {/*
@@ -44,9 +15,7 @@ const categories = [
       ex) /home/shop 의 경우 /home 와 같은 페이지 출력되는데
           이때 Outlet 사용하면 /home/shop의 페이지가 Outlet 범위에 표시됨
     */}
-     
-      
-      <Directory categories={categories} />
+      <Directory />
     </div>
   );
 };
